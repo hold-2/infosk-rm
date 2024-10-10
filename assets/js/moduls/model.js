f18264b154819195de3f45659f9388f56cda256b
lailaboris <liloboris05@gmail.com>
2024-10-10 11:48:08 +0200

    change

--- assets/js/moduls/model.js
+++ assets/js/moduls/model.js
@@ -1,12 +1,11 @@
-const WeatherModel = {
+// model.js
+export const WeatherModel = {
     apiKey: '4d58d6f0a435bf7c5a52e2030f17682d',
-    city: 'Aalborg',
-    apiUrl: function() {
-      return `https://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=${this.apiKey}&units=metric`;
-    },
   
-    fetchWeatherData: function() {
-      return fetch(this.apiUrl())
+    // Funktion til at hente vejret baseret på brugerens koordinater
+    fetchWeatherDataByCoordinates: function(lat, lon) {
+      const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${this.apiKey}&units=metric`;
+      return fetch(apiUrl)
         .then(response => response.json())
         .catch(error => {
           console.error('Fejl ved hentning af vejrdata:', error);
@@ -14,3 +13,4 @@ const WeatherModel = {
         });
     }
   };
+  
--- assets/js/moduls/view.js
+++ assets/js/moduls/view.js
@@ -1,16 +1,24 @@
-const WeatherView = {
+// view.js
+export const WeatherView = {
     updateTime: function(timeString) {
       document.getElementById('time').textContent = `Klokken: ${timeString}`;
+    },
+  
+    updateWeatherDescriptionAndIcon: function(description, icon) {
+      const weatherIconUrl = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+      document.getElementById('description').innerHTML = `
+        <img src="${weatherIconUrl}" alt="Vejr ikon" /><br>
+        Beskrivelse: ${description}
+      `;
     },
   
     updateLocation: function(cityName) {
       document.getElementById('location').textContent = `Lokation: ${cityName}`;
     },
   
-    updateWeather: function(temperature, description) {
+    updateWeather: function(temperature) {
       document.getElementById('weather').innerHTML = `
-        Temperatur: ${temperature}°C<br>
-        Beskrivelse: ${description}
+        Temperatur: ${temperature}°C
       `;
     },
   
@@ -18,5 +26,5 @@ const WeatherView = {
       document.getElementById('weather').textContent = 'Kunne ikke hente vejrdata.';
     }
   };
-
   
+
